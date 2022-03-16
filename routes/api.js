2362dbac34c8bfa4c5d3519ab33a45a156ef099a
David faidley <david.faidley23@gmail.com>
2022-03-16 17:54:19 -0600

    changed around code in api for better flow

--- routes/api.js
+++ routes/api.js
@@ -3,21 +3,21 @@ const fs = require('fs');
 
 // sending the Notes data when the API is called
 router.get('/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf-8', (error, data) => {
-        if (error) throw error;
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) throw err;
         res.json(JSON.parse(data));
     });
 });
 
 // Displaying the notes on that page
 router.post('/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf-8', (error, data) => {
-        if (error) throw error;
-        let working = JSON.parse(data);
-        working.push(req.body);
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        let newNote = JSON.parse(data);
+        newNote.push(req.body);
 
-        fs.writeFile('./db/db.json', JSON.stringify(working), (error) => {
-            if (error) return error;
+        fs.writeFile('./db/db.json', JSON.stringify(newNote), (err) => {
+            if (err) return err;
         });
     });
     res.end();
@@ -26,14 +26,14 @@ router.post('/notes', (req, res) => {
 // deleting notes from the db
 router.delete('/notes/:id', (req, res) => {
     const tobe = req.params.id;
-    fs.readFile('./db/db.json', 'utf-8', (error, data) => {
-        if (error) throw error;
-        let working = JSON.parse(data);
-        for (let i = 0; i < working.length; i++) {
-            if (tobe == working[i].id) {
-                working.splice(i,1);
-                fs.writeFile('./db/db.json', JSON.stringify(working), (error) => {
-                    if (error) throw error;
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        let newNote = JSON.parse(data);
+        for (let i = 0; i < newNote.length; i++) {
+            if (tobe == newNote[i].id) {
+                newNote.splice(i,1);
+                fs.writeFile('./db/db.json', JSON.stringify(newNote), (err) => {
+                    if (err) throw err;
                 });
             };
             
